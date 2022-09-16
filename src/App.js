f896b685c6b1450c6a0a7528be6b2def483988f1
Negin Mahmoudi <negin.mahmoudii76@gmail.com>
2022-09-16 15:21:43 +0200

    Update project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { saveAs } from 'file-saver';
+import { useState } from 'react';
 
 function App() {
+  const [memeTemplate, setMemeTemplate] = useState('');
+  const [uperText, setUperText] = useState('');
+  const [downText, setDownText] = useState('');
+
+  const [url, setUrl] = useState('https://api.memegen.link/images/doge.jpeg');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1> Meme Generator</h1>
+      <div>
+        <img src={url} alt="meme" data-test-id="meme-image" />
+      </div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <label>
+          Meme template
+          <input
+            value={memeTemplate}
+            onChange={(event) => {
+              setMemeTemplate(event.currentTarget.value);
+            }}
+          />
+        </label>
+      </form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <label>
+          Top text
+          <input
+            value={uperText}
+            onChange={(event) => {
+              setUperText(event.currentTarget.value);
+            }}
+          />
+        </label>
+      </form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <label>
+          Bottom text
+          <input
+            value={downText}
+            onChange={(event) => {
+              setDownText(event.currentTarget.value);
+            }}
+          />
+        </label>
+      </form>
+
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <button
+          onClick={() => {
+            setUrl(
+              `https://api.memegen.link/images/${memeTemplate}/${uperText}/${downText}.jpg`,
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Generate
+        </button>
+      </form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <button
+          onClick={() => {
+            saveAs(
+              `https://api.memegen.link/images/${memeTemplate}/${uperText}/${downText}.jpg`,
+              `meme-${memeTemplate}.jpg`,
+            ); // Put your image url here.
+          }}
+        >
+          Download
+        </button>
+      </form>
     </div>
   );
 }
