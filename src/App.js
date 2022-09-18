88b082284be654b4fce32ed42c3a27eb936c2d63
Negin Mahmoudi <negin.mahmoudii76@gmail.com>
2022-09-18 12:21:25 +0200

    Add changes for drone

--- src/App.js
+++ src/App.js
@@ -3,118 +3,72 @@ import { saveAs } from 'file-saver';
 import { useState } from 'react';
 
 function App() {
-  const [memeTemplate, setMemeTemplate] = useState('');
+  const [memeTemplate, setMemeTemplate] = useState('doge');
   const [uperText, setUperText] = useState('');
   const [downText, setDownText] = useState('');
-
   const [url, setUrl] = useState('https://api.memegen.link/images/doge.jpeg');
 
-  function urlConstruct(memeType, topText, bottomText) {
-    if (memeType === '') {
-      memeType = 'doge';
-    }
-    if (topText !== '') {
-      topText = `/${topText}`;
-    } else {
-      topText = `/_`;
-    }
-    if (bottomText !== '') {
-      bottomText = `/${bottomText}`;
-    }
-    return `${memeType}${topText}${bottomText}`;
-  }
-  console.log(urlConstruct(memeTemplate, uperText, downText));
   return (
     <div className="container">
       <h1> Meme Generator</h1>
       <div>
-        <img src={url} alt="meme" data-test-id="meme-image" />
+        <img data-test-id="meme-image" src={url} alt="meme" />
       </div>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <label>
-          Meme template
-          <input
-            value={memeTemplate}
-            onChange={(event) => {
-              setMemeTemplate(event.currentTarget.value);
-            }}
-          />
-        </label>
-      </form>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <label>
-          Top text
-          <input
-            value={uperText}
-            onChange={(event) => {
-              setUperText(event.currentTarget.value);
-            }}
-          />
-        </label>
-      </form>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <label>
-          Bottom text
-          <input
-            value={downText}
-            onChange={(event) => {
-              setDownText(event.currentTarget.value);
-            }}
-          />
-        </label>
-      </form>
+      <label>
+        Meme template
+        <input
+          value={memeTemplate}
+          onChange={(event) => {
+            setMemeTemplate(event.currentTarget.value);
+          }}
+        />
+      </label>
 
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
+      <label>
+        Top text
+        <input
+          value={uperText}
+          onChange={(event) => {
+            setUperText(event.currentTarget.value);
+          }}
+        />
+      </label>
+
+      <label>
+        Bottom text
+        <input
+          value={downText}
+          onChange={(event) => {
+            setDownText(event.currentTarget.value);
+          }}
+        />
+      </label>
+
+      <button
+        onClick={() => {
+          setUrl(function () {
+            if (uperText.length > 0) {
+              return `https://api.memegen.link/images/${memeTemplate}/${uperText}/${downText}.jpg`;
+            } else {
+              return `https://api.memegen.link/images/${memeTemplate}/_/${downText}.jpg`;
+            }
+          });
         }}
       >
-        <button
-          onClick={() => {
-            setUrl(
-              `https://api.memegen.link/images/${urlConstruct(
-                memeTemplate,
-                uperText,
-                downText,
-              )}.jpg`,
-            );
-          }}
-        >
-          Generate
-        </button>
-      </form>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
+        Generate
+      </button>
+
+      <button
+        onClick={() => {
+          saveAs(
+            // Put your image url here.
+            url,
+            `meme-${memeTemplate}.jpg`,
+          );
         }}
       >
-        <button
-          onClick={() => {
-            saveAs(
-              `https://api.memegen.link/images/${urlConstruct(
-                memeTemplate,
-                uperText,
-                downText,
-              )}.jpg`,
-              `meme-${memeTemplate}.jpg`,
-            ); // Put your image url here.
-          }}
-        >
-          Download
-        </button>
-      </form>
+        Download
+      </button>
     </div>
   );
 }
